61245d164578113d92114e9742fe14e3882c5cd7
skies-codes <akashdebnathwd@gmail.com>
2024-09-28 16:08:03 +0530

    stage 3: Integrating Socket.io

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from "express";
 import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -12,6 +14,15 @@ app.use("/assets", express.static(join(__dirname, "public/assets")));
 
 app.get("/", (req, res) => {
     res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+    // console.log(socket);
+    console.log(`a user is connected ${socket.id}`);
+
+    socket.on("disconnect", () => {
+        console.log(`user disconnected ${socket.id}`);
+    });
 });
 
 server.listen(3000, () => {
